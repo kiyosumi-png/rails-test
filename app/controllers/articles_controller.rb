8bd02855aabc53a64c40c6c0bbe07415e3bb747d
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2023-02-18 11:47:56 +0900

    create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,21 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_paprams)
+    
+    @article.save
+    redirect_to action: :index
+  end
+
+  private
+    def article_paprams
+      params.require(:article).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index" 
+  get "/articles/add", to: "articles#new"
+  post "/articles", to: "articles#create"
 end
