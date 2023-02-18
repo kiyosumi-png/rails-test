7c0348001c7da444af0e3e1cf6f3139157654038
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2023-02-18 14:25:00 +0900

    find

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index" 
+  get "/articles/:id", to: "articles#show"
+
   get "/articles/add", to: "articles#new"
   post "/articles", to: "articles#create"
 end
