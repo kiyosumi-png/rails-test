02980baa9971bba8ed03a9c784100ab30a355cf8
kiyosumi-png <shoudaoqingcheng3@gmail.com>
2023-03-21 14:32:02 +0900

    application

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "books#index"
 
-  get "books/new", to: "books#new"
+  get "books/new", to: "books#new", as: "new_book"
   post "/books", to: "books#create"
 
-  get "books/:id/edit", to: "books#edit"
+  get "books/:id", to: "books#show", as: "detail_book"
+
+  get "books/:id/edit", to: "books#edit", as: "edit_book"
   patch "books/:id", to: "books#update", as: "book"
 
   delete "books/:id/delete", to: "books#destroy", as: "delete_book"
